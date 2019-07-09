a1268df2aa230610fd9be1e14dfcc975d32fc118
Nitin Nepalia <nitin.nepalia@gmail.com>
2019-07-09 17:08:41 +0530

    Delete Remove_spaces.groovy

--- Remove_spaces.groovy
+++
@@ -1,8 +0,0 @@
-String readFileString() {
-    File file = new File("Desktop/newfile.txt")
-    File file2 = new File("Desktop/out.txt")
-    String fileContent = file.text
-    fileContent = fileContent*.trim().join('')
-    file2.write fileContent
-}
-readFileString()
